24769ec6610fe8bbbd9c3cb8fc98e5c320d4a2b8
jannierfg <52221862+jannierfg@users.noreply.github.com>
2020-07-21 14:58:35 -0500

    home llama component confirmar servicio

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import{GlobalsService} from '../../globals.service';
-import {Router} from '@angular/router';
-import {  ViewChild,Directive } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { GlobalsService } from '../../globals.service';
+import { Router } from '@angular/router';
+import { ViewChild, Directive } from '@angular/core';
 import { Address } from 'ngx-google-places-autocomplete/objects/address';
-import {MapComponent} from './map/map.component';
-import {MatDialog} from '@angular/material/dialog';
-import {ApiRegisterService} from '../../services/apiRegister/api-register.service';
-import {HeaderComponent} from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { ConfimarServicioComponent } from './confimar-servicio/confimar-servicio.component';
+// import {ApiRegisterService} from '../../services/apiRegister/api-register.service';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-home',
@@ -17,19 +18,22 @@ import {HeaderComponent} from './header/header.component';
 })
 export class HomeComponent implements OnInit {
 
-  @ViewChild(MapComponent) public hijo:MapComponent;
-  constructor(public globals:GlobalsService, private router: Router, public servi:ApiRegisterService) { }
+  @ViewChild(MapComponent) public hijo: MapComponent;
+  constructor(public globals: GlobalsService, private router: Router,
+    public dialog: MatDialog
+    // , public servi:ApiRegisterService
+  ) { }
   tamanoPaquete: any[] = [];
   tipoServicio: any[] = [];
   panelOpenState = false;
-  nombre=this.globals.name;
-  id_user=this.globals.id;
+  nombre = this.globals.name;
+  id_user = this.globals.id;
   solicitado = false;
-  resumen=false;
-  especificaciones="reger";
-  especificaciones2="erwtert";
+  resumen = false;
+  especificaciones = "reger";
+  especificaciones2 = "erwtert";
   options = {
-    types : [],
+    types: [],
     componentRestrictions: { country: 'CO' }
   }
   ngOnInit(): void {
@@ -40,27 +44,27 @@ export class HomeComponent implements OnInit {
     this.tipoServi();
 
   }
-  logout(){
+  logout() {
     this.globals.logout();
     this.router.navigate(['']);
   }
-  solicitar(){
-    this.solicitado=true;
-    this.resumen=true;
+  solicitar() {
+    this.solicitado = true;
+    this.resumen = true;
     this.pedido();
   }
-  cancelar(){
-    this.solicitado=false;
-    this.resumen=false;
+  cancelar() {
+    this.solicitado = false;
+    this.resumen = false;
   }
-  doAddressChange(address:Address){
+  doAddressChange(address: Address) {
     this.hijo.handleAddressChange(address);
   }
-  doAddressChangeOrigin(address:Address){
+  doAddressChangeOrigin(address: Address) {
     this.hijo.handleAddressChangeOrigin(address);
   }
   listar() {
-    this.servi.tamano(this, this.servi.urls.tamanoPaquete, this.succes, this.errorHanndler);
+    // this.servi.tamano(this, this.servi.urls.tamanoPaquete, this.succes, this.errorHanndler);
   }
 
   succes(_this, data) {
@@ -72,7 +76,7 @@ export class HomeComponent implements OnInit {
     console.log("error " + data.error.message);
   }
   tipoServi() {
-    this.servi.tipo(this, this.servi.urls.tamanoPaquete, this.succesTipo, this.errorHanndlerTipo);
+    // this.servi.tipo(this, this.servi.urls.tamanoPaquete, this.succesTipo, this.errorHanndlerTipo);
   }
 
   succesTipo(_this, data) {
@@ -86,28 +90,28 @@ export class HomeComponent implements OnInit {
 
   pedido() {
     let data = {
-      "service_type":1,
-      "package_size":1,
-      "distanace_traveled":this.hijo.distancia,
-      "total_value":this.hijo.valor,
-      "qualification":0,
-      "client_id":this.id_user,
-      "list_address":[{
-        "latitude":10,
-        "longitude":10,
-        "address":this.hijo.dirOrigen,
-        "specification":this.especificaciones,
-        "instructions":"casa de esquina"
-      },{
-        "latitude":10,
-        "longitude":10,
-        "address":this.hijo.dirDestino,
-        "specification":this.especificaciones2,
-        "instructions":"casa de esquina"
+      "service_type": 1,
+      "package_size": 1,
+      "distanace_traveled": this.hijo.distancia,
+      "total_value": this.hijo.valor,
+      "qualification": 0,
+      "client_id": this.id_user,
+      "list_address": [{
+        "latitude": 10,
+        "longitude": 10,
+        "address": this.hijo.dirOrigen,
+        "specification": this.especificaciones,
+        "instructions": "casa de esquina"
+      }, {
+        "latitude": 10,
+        "longitude": 10,
+        "address": this.hijo.dirDestino,
+        "specification": this.especificaciones2,
+        "instructions": "casa de esquina"
       }]
     };
     console.log(data.client_id);
-    this.servi.pedido(this, data, this.succesPedido, this.errorHanndlerPedido);
+    // this.servi.pedido(this, data, this.succesPedido, this.errorHanndlerPedido);
   }
 
   succesPedido(_this, data) {
@@ -118,5 +122,21 @@ export class HomeComponent implements OnInit {
 
   errorHanndlerPedido(_this, data) {
     console.log("error " + data.error.message);
+  }
+
+  confirmarServicio() {
+    const dialogRef = this.dialog.open(ConfimarServicioComponent, {
+      width: '400px',
+      data: { mode: 'create' }
+    });
+
+    dialogRef.afterClosed()
+      .subscribe((confirmado: Boolean) => {
+        if (!confirmado) {
+          console.log('Cerrado');
+        } else {
+          console.log('Guardado');
+        }
+      });
   }
 }
