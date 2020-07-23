b37f850215cef9b66b19c7a54eeefaed2e837576
jannierfg <52221862+jannierfg@users.noreply.github.com>
2020-07-22 19:21:24 -0500

    cambios componente perfil

--- src/app/components/home/profile/profile.component.ts
+++ src/app/components/home/profile/profile.component.ts
@@ -135,4 +135,19 @@ export class ProfileComponent implements OnInit {
         }
       });
   }
+
+  codigo() {
+    console.log('codigo')
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById('copy').innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+
+    var msj = document.getElementById('msj-copy')
+    msj.style.display = 'block'
+
+    setTimeout(function () { msj.style.display = 'none'; }, 3000);
+  }
 }
