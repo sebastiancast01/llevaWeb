17c92d4796dfd98325fbec77960d7d1ef91b5b87
jannierfg <52221862+jannierfg@users.noreply.github.com>
2020-07-25 15:25:12 -0500

    cambios confirmar numero de celular y cargar documentos

--- src/app/components/phone/confirm/confirm.component.ts
+++ src/app/components/phone/confirm/confirm.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 // import {ApiRegisterService} from '../../../services/apiRegister/api-register.service';
-import {Router} from '@angular/router';
-import {MatSnackBar} from '@angular/material/snack-bar';
-import{GlobalsService} from '../../../globals.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { GlobalsService } from '../../../globals.service';
 
 @Component({
   selector: 'app-confirm',
@@ -13,17 +13,17 @@ export class ConfirmComponent implements OnInit {
 
   constructor(
     // public apiRegister: ApiRegisterService,
-     public router: Router, private _snackBar: MatSnackBar,
-              public globals:GlobalsService) { }
-  phone=this.globals.phone;
+    public router: Router, private _snackBar: MatSnackBar,
+    public globals: GlobalsService) { }
+  phone = this.globals.phone;
 
   code_pais = "57";
-  n1=null;
-  n2=null;
-  n3=null;
-  n4=null;
-  n5=null;
-  sms_code=null;
+  n1 = null;
+  n2 = null;
+  n3 = null;
+  n4 = null;
+  n5 = null;
+  sms_code = null;
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
       duration: 2000,
@@ -41,10 +41,25 @@ export class ConfirmComponent implements OnInit {
 
   }
 
+  onKey(event, id) {
+    console.log(event)
+    console.log('input' + (parseFloat(id) + parseFloat('1')))
 
+    if ((parseFloat(id) + parseFloat('1') > 5)) {
+      console.log('No se puede')
+    } else {
+      var idInput = 'input' + (parseFloat(id) + parseFloat('1'))
+
+      var input = document.getElementById(idInput)
+
+      input.focus();
+    }
+
+
+  }
 
   reenviarCodigo(_this, data) {
-    _this.openSnackBar("Codigo enviado Nuevamente","");
+    _this.openSnackBar("Codigo enviado Nuevamente", "");
 
   }
 
@@ -52,14 +67,14 @@ export class ConfirmComponent implements OnInit {
   }
   validarCodigo() {
     //this.confirm.set_phone(this.phone);
-    this.sms_code = this.n1+""+this.n2+""+this.n3+""+this.n4+""+this.n5;
+    this.sms_code = this.n1 + "" + this.n2 + "" + this.n3 + "" + this.n4 + "" + this.n5;
 
     let data = {
       'code_pais': this.code_pais,
       'phone': this.phone,
       'sms_code': this.sms_code
     };
-    let phone= this.globals.phone;
+    let phone = this.globals.phone;
 
     // this.apiRegister.valCodePhone(this, data, this.despuesDeEnviarCodigo, this.errorHanndler);
   }
@@ -68,12 +83,12 @@ export class ConfirmComponent implements OnInit {
 
   despuesDeEnviarCodigo(_this, data) {
     console.log("Codigo confirmado" + data);
-    _this.globals.sms_code=_this.sms_code;
-    _this.globals.secret_phone=data.secrect_phone;
-    if(_this.globals.tipo_registro==1){
+    _this.globals.sms_code = _this.sms_code;
+    _this.globals.secret_phone = data.secrect_phone;
+    if (_this.globals.tipo_registro == 1) {
       _this.router.navigate(['/register-person']);
 
-    }else{
+    } else {
       _this.router.navigate(['/register-empresa']);
 
     }
@@ -81,11 +96,11 @@ export class ConfirmComponent implements OnInit {
 
   errorHanndler(_this, data) {
     console.log("error " + data.error.message);
-    _this.openSnackBar("Error",data.error.message);
+    _this.openSnackBar("Error", data.error.message);
   }
-    ngOnInit(): void {
+  ngOnInit(): void {
   }
-  set_phone(phone){
-    this.phone=phone;
+  set_phone(phone) {
+    this.phone = phone;
   }
 }
